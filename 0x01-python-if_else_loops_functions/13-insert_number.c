492f5f736fa70c0b9d4d7872aedfea175559be78
NahlaHussein1 <nahlahussein518@gmail.com>
2023-08-11 19:15:44 +0000

    s

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,13 +1,44 @@
-#!/usr/bin/python3
+#include "lists.h"
+#include <stdlib.h>
+#include <unistd.h>
 
+/**
+ * insert_node - inserts a number in an ordered linked list
+ * @head: double pointer to the linked list
+ * @number: number to insert in the new node
+ *
+ * Return: address of the new node, or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head;
+	listint_t *new = NULL;
+	listint_t *temp = NULL;
 
-def fizzbuzz():
-    for m in range(1, 101):
-        if m % 3 == 0 and m % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif m % 3 == 0:
-            print("Fizz ", end="")
-        elif m % 5 == 0:
-            print("Buzz ", end="")
-        else:
-            print("{} ".format(m), end="")
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	if (!*head || (*head)->n > number)
+	{
+		new->next = *head;
+		return (*head = new);
+	}
+	else
+	{
+		while (current && current->n < number)
+		{
+			temp = current;
+			current = current->next;
+		}
+		temp->next = new;
+		new->next = current;
+	}
+
+	return (new);
+}
