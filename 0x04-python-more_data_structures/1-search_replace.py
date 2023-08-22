edf44306149a96dd3c602f7921c8c9b8382ef6ae
NahlaHussein1 <nahlahussein518@gmail.com>
2023-08-22 19:31:27 +0000

    d

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,12 @@
-earch_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-    return new_list
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    a function that replaces or adds key/value in a dictionary.
+    key argument will be always a string
+    value argument will be any type
+    """
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
